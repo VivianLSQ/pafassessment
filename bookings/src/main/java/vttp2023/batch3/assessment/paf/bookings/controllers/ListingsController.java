915c4efaeed52f1dec3628af59f87fac0f2c364f
VivianLim <vivianlimsq@gmail.com>
2023-07-21 11:00:31 +0800

    edits made

--- bookings/src/main/java/vttp2023/batch3/assessment/paf/bookings/controllers/ListingsController.java
+++ bookings/src/main/java/vttp2023/batch3/assessment/paf/bookings/controllers/ListingsController.java
@@ -1,9 +1,18 @@
 package vttp2023.batch3.assessment.paf.bookings.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class ListingsController {
 
 	//TODO: Task 2
-
+	@GetMapping(@ModelAttribute ){
+		
+		return "landingPage"; 
+	}
 	
 	//TODO: Task 3
 
---
+++ bookings/src/main/java/vttp2023/batch3/assessment/paf/bookings/models/User.java
@@ -0,0 +1,50 @@
+package vttp2023.batch3.assessment.paf.bookings.models;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+
+public class User {
+
+    @NotEmpty(message="This field is mandatory")
+    @NotNull(message= "Please indicate your destination country")
+    private String country; 
+
+    @NotEmpty(message="This field is mandatory")
+    @Min(value =1, message = "Party size must be one or greater")
+    @Max(value=10, message="Party size must not exceed 10")
+    private int numberOfPerson;
+
+    @DecimalMin(value = "1.00", inclusive = true, message = "Please indicate price range must be greater than 1")
+    @DecimalMax(value = "10000.00", inclusive = true, message = "Please indicate price range must be less than than 10,000")
+    private float priceRange; 
+    
+
+     public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getNumberOfPerson() {
+        return numberOfPerson;
+    }
+
+    public void setNumberOfPerson(int numberOfPerson) {
+        this.numberOfPerson = numberOfPerson;
+    }
+
+    public float getPriceRange() {
+        return priceRange;
+    }
+
+    public void setPriceRange(float priceRange) {
+        this.priceRange = priceRange;
+    }
+}
